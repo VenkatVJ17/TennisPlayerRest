700cebef8f328d858ff167a65446c8d59f3b8179
VenkatVJ17 <venkat.jalaneela@hotmail.com>
2024-06-10 19:19:29 -0500

    Initial commit rest

---
+++ src/main/java/com/venkatscode/restapi/tennis_player_rest/PlayerController.java
@@ -0,0 +1,54 @@
+package com.venkatscode.restapi.tennis_player_rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class PlayerController {
+
+    @Autowired
+    PlayerService service;
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Tennis Player REST API";
+    }
+
+    @GetMapping("/players")
+    public List<Player> allPlayers() {
+        return service.getAllPlayers();
+    }
+    @GetMapping("/players/{id}")
+    public Player getPlayer(@PathVariable int id){
+        return service.getPlayer(id);
+    }
+
+    @PostMapping("/players")
+    public Player addPlayer(@RequestBody Player player) {
+        player.setId(0);
+        return service.addPlayer(player);
+    }
+
+    @PutMapping("/players/{id}")
+    public Player updatePlayer(@PathVariable int id, @RequestBody Player player) {
+        return service.updatePlayer(id, player);
+    }
+
+    @PatchMapping("/players/{id}")
+    public Player partialUpdate(@PathVariable int id, @RequestBody Map<String, Object> playerPatch) {
+        return service.patch(id, playerPatch);
+    }
+
+    @PatchMapping("/players/{id}/titles")
+    public void updateTitles(@PathVariable int id, @RequestBody int titles) {
+        service.updateTitles(id, titles);
+    }
+
+    @DeleteMapping("/players/{id}")
+    public void deletePlayer(@PathVariable int id) {
+        service.deletePlayer(id);
+    }
+}
---
+++ src/main/java/com/venkatscode/restapi/tennis_player_rest/PlayerService.java
@@ -0,0 +1,89 @@
+package com.venkatscode.restapi.tennis_player_rest;
+
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ReflectionUtils;
+
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Service
+public class PlayerService {
+
+    @Autowired
+    PlayerRepository repo;
+
+    //Get all players
+    public List<Player> getAllPlayers() {
+        return repo.findAll();
+    }
+
+    //Get player by ID
+    public Player getPlayer(int id) {
+        Optional<Player> tempPlayer = repo.findById(id);
+
+        if(tempPlayer.isEmpty())
+            throw new PlayerNotFoundException("Player with id {"+ id +"} not found");
+
+        return tempPlayer.get();
+    }
+
+    //Add a player
+    public Player addPlayer(Player p) {
+        return repo.save(p);
+    }
+
+    //Update a player
+    public Player updatePlayer(int id, Player p) {
+        Optional<Player> tempPlayer = repo.findById(id);
+
+        if(tempPlayer.isEmpty())
+            throw new PlayerNotFoundException("Player with id {"+ id +"} not found");
+
+        p.setId(id);
+        return repo.save(p);
+    }
+
+    //Partial update
+    public Player patch( int id, Map<String, Object> partialPlayer) {
+
+        Optional<Player> player = repo.findById(id);
+
+        if(player.isPresent()) {
+            partialPlayer.forEach( (key, value) -> {
+                System.out.println("Key: " + key + " Value: " + value);
+                Field field = ReflectionUtils.findField(Player.class, key);
+                ReflectionUtils.makeAccessible(field);
+                ReflectionUtils.setField(field, player.get(), value);
+            });
+        }
+        else
+            throw new PlayerNotFoundException("Player with id {"+ id +"} not found");
+
+        return repo.save(player.get());
+    }
+
+    //delete a player
+    public void deletePlayer(int id) {
+        Optional<Player> tempPlayer = repo.findById(id);
+
+        if(tempPlayer.isEmpty())
+            throw new PlayerNotFoundException("Player with id {"+ id +"} not found");
+
+        repo.delete(tempPlayer.get());
+    }
+
+    //update a single field
+    @Transactional
+    public void updateTitles(int id, int titles) {
+        Optional<Player> tempPlayer = repo.findById(id);
+
+        if(tempPlayer.isEmpty())
+            throw new PlayerNotFoundException("Player with id {"+ id +"} not found");
+
+        repo.updateTitles(id, titles);
+    }
+}
